2444f6a4f820820b25f875a6d8da4fe733cc7296
Asko Kauppi <akauppi@gmail.com>
2021-06-01 08:48:09 +0300

    more simplifications; removing functions emulation since it's not needed; correcting the polarity of the title of the test

--- functions/tests/example.test.js
+++ functions/tests/example.test.js
@@ -5,10 +5,11 @@ describe("Security rules", () => {
         await Promise.all(firebase.apps().map((app) => app.delete()));
     });
 
-    it("Can read stuff", async () => {
+    it("should not be allowed to read stuff", async () => {
         const app = firebase.initializeTestApp({ projectId: "whatever" });
         const db = app.firestore();
         const testDoc = db.collection("private").doc("a_doc");
+
         await firebase.assertFails(testDoc.get());
     });
 });
